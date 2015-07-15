8a07f841f8892dc039913819df1b280aa91fe222
Gabriel Fernández Moral <gabriel.fernandez.moral@gmail.com>
2015-07-15 23:34:59 +0200

    setting up express

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,10 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { 
+  	title: 'Bienvenido a Quiz', 
+  	subtitle: 'El portal donde podrá crear sus propios juegos!' 
+  });
 });
 
 module.exports = router;
