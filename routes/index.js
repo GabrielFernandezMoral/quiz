4ca66e7eca75d1aa256095f336f733c24060af8a
Gabriel Fernández Moral <gabriel.fernandez.moral@gmail.com>
2015-08-05 11:41:17 +0200

    Multiple questions supported

--- routes/index.js
+++ routes/index.js
@@ -8,8 +8,13 @@ router.get('/', function(req, res, next) {
   res.render('index');
 });
 
-router.get('/quizes/question', quizController.question);
-router.get('/quizes/answer', quizController.answer);
-router.get('/author', quizController.author);
+// Autoload de comandos con :quizId
+router.param('quizId', quizController.load); // autoload :quizId
+
+// Definición de rutas de /quizes
+router.get('/quizes', 						quizController.index);
+router.get('/quizes/:quizId(\\d+)', 		quizController.show);
+router.get('/quizes/:quizId(\\d+)/answer', 	quizController.answer);
+router.get('/author', 						quizController.author);
 
 module.exports = router;
